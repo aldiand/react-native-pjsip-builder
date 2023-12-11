3f0c708aea1f5f73c4680c2ebece3cd5bd78b506
Andre Bahtiar Fauzi <supersandre@gmail.com>
2023-12-11 19:24:22 +0700

    update ios build

--- build_ios.sh
+++ build_ios.sh
@@ -10,6 +10,10 @@ mkdir -p ./dist/;
 docker build -t react-native-pjsip-builder/ios ./ios/;
 docker run --name ${CONTAINER_NAME} ${IMAGE_NAME} bin/true
 
-docker cp ${CONTAINER_NAME}:/dist/ios ./dist/ios
+docker cp ${CONTAINER_NAME}:/dist/ios.tar.gz ./dist/ios.tar.gz
 
 docker rm ${CONTAINER_NAME}
+
+tar -xzf ./dist/ios.tar.gz -C ./dist/
+
+rm ./dist/ios.tar.gz
